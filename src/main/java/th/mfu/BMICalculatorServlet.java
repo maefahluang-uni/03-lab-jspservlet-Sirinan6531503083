a8b2ed9fc1a28dd8228e3588f79c3b49a5322096
Sirinan6531503083 <122779571+Sirinan6531503083@users.noreply.github.com>
2023-09-04 09:03:37 +0000

    6531503083

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -9,53 +9,41 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
+@WebServlet("/calbmi")
 public class BMICalculatorServlet extends HttpServlet{
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //TODO: get parameter from request: "weight" and "height"
-        String weightStr = request.getParameter("weight");
-        String heightStr = request.getParameter("height");
-
-        try {
+        double weight = Double.parseDouble(request.getParameter("weight"));
+        double height = Double.parseDouble(request.getParameter("height"));
         //TODO: calculate bmi
-        double weight = Double.parseDouble(weightStr);
-        double height = Double.parseDouble(heightStr);
-
-        double bmi = calculateBMI(weight, height);
+        double bmi = weight / ((height ) * (height ));
+        int roundedBMI = (int) Math.round(bmi);
 
         //TODO: determine the built from BMI
-        String builtType = determineBuiltType(bmi);
+        String builtType ="normal";
+        if (bmi < 18.5) {
+            builtType = "underweight";
+        } else if (bmi < 25) {
+            builtType = "normal";
+        } else if (bmi < 30) {
+            builtType = "overweight";
+        } else if (bmi < 35) {
+            builtType = "obese";
+        } else {
+            builtType = "extremely obese";
+        }
       
         //TODO: add bmi and built to the request's attribute
-        request.setAttribute("bmi", bmi);
+        request.setAttribute("bmi", roundedBMI);
         request.setAttribute("builtType", builtType);
+
+        
 
         //TODO: forward to jsp
         request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
-        } catch (NumberFormatException e) {
-            request.setAttribute("error", "Invalid input. Please enter numeric values for weight and height.");
-            request.getRequestDispatcher("/error.jsp").forward(request, response);
-        }
+           
     }
-    private double calculateBMI(double weight, double height) {
-        // Calculate BMI (as an integer without decimal points)
-        double bmi = (weight / (height * height));
-        return Math.round(bmi);
-    }
-
-    private String determineBuiltType(double bmi) {
-        if (bmi < 18.5) {
-            return "Underweight";
-        } else if (bmi < 25) {
-            return "Normal";
-        } else if (bmi < 30) {
-            return "Overweight";
-        } else if (bmi < 35) {
-            return "Obese";
-        } else {
-            return "Extremely Obese";
-        }
-    }
+    
 }
-
